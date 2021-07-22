32cdc20836a8c645034235674a12e660e736bc03
sammidunn <sammidunn@hotmail.com>
2021-07-22 18:53:45 +0000

    cleared text fields on submit and checked for caps in passcode

--- public/js/writeMessage.js
+++ public/js/writeMessage.js
@@ -1,11 +1,33 @@
 const onSubmit = () => {
-    const passcodeInput = document.querySelector("#passcode").value;
-    const messageInput = document.querySelector("#message").value;
-   
-    const payload = {
-        passcode: passcodeInput,
-        message: messageInput
-    };
+    const passcodeField = document.querySelector("#passcode");
+    const passcodeInput = passcodeField.value;
+    const messageField = document.querySelector("#message");
+    const messageInput = messageField.value;
     
-    firebase.database().ref().push(payload);
+    if(checkPasscode(passcodeInput)) {
+        const payload = {
+            passcode: passcodeInput,
+            message: messageInput
+        };
+
+        firebase.database().ref().push(payload);
+    } else {
+        alert("Passcode must be one word and contain a capital letter!");
+    }
+
+    passcodeField.value = "";
+    messageField.value = "";
+}
+
+const checkPasscode = (input) => {
+    if(input.includes(" ")) {
+        return false;
+    }
+
+    for(let c of input) {
+        if(c === c.toUpperCase()) {
+            return true;
+        }
+    }
+    return false;
 }
