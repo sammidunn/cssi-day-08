f5f3344cb6a3acd3d68487c1927bfc52ff681860
sammidunn <sammidunn@hotmail.com>
2021-07-22 16:54:07 +0000

    logging all keys and values

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -1,9 +1,13 @@
 const getMessages = () => {
     const passcode = document.querySelector("#passcode");
-    
+  
     const messagesRef = firebase.database().ref();
     messagesRef.on('value', (snapshot) => {
         const data = snapshot.val();
         console.log(data);
+
+        for(let key in data) {
+            console.log(key, data[key]);
+        }
     })
 }
